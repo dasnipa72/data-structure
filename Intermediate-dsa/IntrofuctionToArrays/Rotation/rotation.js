a8411d82d8788e14c083761f2e029c1c0425e95b
Nipa Das <nipa.das@niveussolutions.com>
2022-12-17 15:53:23 +0530

    ds - array rotation - optimized

--- Intermediate-dsa/IntrofuctionToArrays/Rotation/rotation.js
+++ Intermediate-dsa/IntrofuctionToArrays/Rotation/rotation.js
@@ -5,23 +5,30 @@ function Rotation(A, B){
      return A
 }
 
-function ReverseArray(A){
-    let revArr = []
-    for(let i=A.length-1, j=0; i>=0, j<A.length; i--, j++){
-        revArr[j]=A[i]
-    }
-    return revArr;
-}
+// function ReverseArray(A){
+//     let revArr = []
+//     for(let i=A.length-1, j=0; i>=0, j<A.length; i--, j++){
+//         revArr[j]=A[i]
+//     }
+//     return revArr;
+// }
 function ReverseArrayApproach2(A, start, end){
-    let temp;
+    // let temp;
     while(start<end){
-        temp = A[start];
-        A[start]=A[end];
-        A[end]= temp
+        // temp = A[start];
+        // A[start]=A[end];
+        // A[end]= temp
+        Swap(A, start, end)
         start++;
         end--;
     }
     return A;
+}
+
+function Swap(Arr, A, B){
+    Arr[A] = Arr[A] + Arr[B]
+    Arr[B] = Arr[A]- Arr[B]
+    Arr[A] = Arr[A]- Arr[B]
 }
 
 // console.log(ReverseArray([4,2,6,3,1]));
